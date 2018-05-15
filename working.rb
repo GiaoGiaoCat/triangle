33ec218d0ce9d78ca15357df38da584a101e4916
Victor Wang <wjp2013@gmail.com>
2018-05-15 23:39:08 +0800

    stash code

--- working.rb
+++ working.rb
@@ -1,40 +1,58 @@
 #!/usr/bin/env ruby
 require_relative 'lib/sludge'
 
-# SYMBOLS = %w[
+# CURRENCIES = %w[
 #   btc bch eth etc ltc eos xrp omg dash zec ada act btm bts ht trx neo qtum ela ven snt nas hsr elf gnt
 # ]
 
-SYMBOLS = %w[
-  bch
+CURRENCIES = %w[
+  bch eth etc ltc eos xrp omg dash
 ]
 
-client = Sludge::Client.new
-# p client.symbols
 
-file = File.read "symbols.json"
-data = JSON.parse(file)
+def initialize_data
+  @client = Sludge::Client.new
 
-new_symbols = []
+  file = File.read "symbols.json"
+  data = JSON.parse(file)
 
-SYMBOLS.each do |symbol|
-  data["data"].each do |symbol_data|
-    new_symbols << symbol_data if symbol_data["base-currency"] == symbol
+  @new_symbols = []
+
+  CURRENCIES.each do |currency|
+    data["data"].each do |symbol_data|
+      @new_symbols << symbol_data if symbol_data["base-currency"] == currency
+    end
   end
 end
 
-# puts new_symbols
-btc_usdt = client.get_market_depth('btcusdt')
-if btc_usdt.status
-  p2 = btc_usdt.asks[0][0].to_f
+
+def get_btc_price
+  btc_usdt = @client.get_market_depth('btcusdt')
+  btc_usdt.asks[0][0].to_f if btc_usdt.status
 end
 
-SYMBOLS.each do |symbol|
-  p1 = client.get_market_depth("#{symbol}usdt").bids[0][0].to_f
-  p p1
-  p3 = p1/p2
-  p p3
-  p4 = client.get_market_depth("#{symbol}btc").asks[0][0].to_f
-  p p4
-  sleep 5
+def buy_btc(price)
+
 end
+
+def working
+  initialize_data
+
+  # loop do
+  #
+  # end
+
+  p2 = get_btc_price
+
+  CURRENCIES.each do |currency|
+    p1 = @client.get_market_depth("#{currency}usdt").bids[0][0].to_f
+    usdt_symbol = @new_symbols.find { |symbol| symbol["base-currency"] == currency && symbol["quote-currency"] == "btc" }
+    p3 = ((p1 * 0.998)/(p2/0.998)).round(usdt_symbol["price-precision"])
+
+    p4 = (@client.get_market_depth("#{currency}btc").asks[0][0].to_f * 0.998).round(usdt_symbol["price-precision"])
+    puts "#{currency} #{p3}, #{p4}"
+  end
+end
+
+
+working
